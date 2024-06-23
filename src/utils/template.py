43c0086f7699290a34df1450ec66c9048797dbb6
Tong Zhu (朱桐) <tzhu1997@outlook.com>
2024-06-23 10:08:43 +0800

    Update template.py
    fix the mismatched system prompts according to #9

--- src/utils/template.py
+++ src/utils/template.py
@@ -18,7 +18,7 @@ register_conv_template(
 
 register_conv_template(
     Conversation(
-        name="vicuna_v1.1_aug",
+        name="vicuna_v1.1_seed",
         system_message=(
             "This is a chat between a curious user and a helpful artificial intelligence assistant. "
             "Given the assistant's reponse, please predict the user's instruction in the style of an AI Assistant."
@@ -34,7 +34,7 @@ register_conv_template(
 
 register_conv_template(
     Conversation(
-        name="vicuna_v1.1_seed",
+        name="vicuna_v1.1_aug",
         system_message=(
             "This is a chat between a curious user and a helpful artificial intelligence assistant. "
             "The assistant gives helpful, detailed, and polite answers to the user's questions with knowledge from web search."
